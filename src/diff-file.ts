5a9c816e965971812bc9b126d279851b1bb35274
Travis Arnold <travis@replay.io>
2022-08-22 16:08:01 -0700

    don't allow null values for diffFile

--- src/diff-file.ts
+++ src/diff-file.ts
@@ -2,11 +2,8 @@ import { FileResponse } from 'figma-js'
 import { diff } from 'jest-diff'
 
 /** Determine changes between two file responses returned from [getFile]. */
-export function diffFile(
-  fileA: FileResponse | null,
-  fileB: FileResponse | null
-) {
-  return diff(fileA ?? {}, fileB ?? {}, {
+export function diffFile(fileA: FileResponse, fileB: FileResponse) {
+  return diff(fileA, fileB, {
     contextLines: 0,
     expand: false,
     omitAnnotationLines: true,
