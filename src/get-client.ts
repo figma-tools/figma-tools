3101f7be98b8cefe91da9a7aeac5d28e32594a0e
Travis Arnold <ftntravis@gmail.com>
2023-07-25 12:48:08 -0700

    add error handling when token is not defined

--- src/get-client.ts
+++ src/get-client.ts
@@ -4,6 +4,16 @@ let client = null
 
 export function getClient(): Figma.ClientInterface {
   if (client === null) {
+    if (!process.env.FIGMA_TOKEN) {
+      throw new Error(
+        [
+          'FIGMA_TOKEN environment variable is not set.',
+          'You must include a personal access token in a .env file at the root of your project or as an environment variable.',
+          'Learn more: https://www.figma.com/developers/docs#auth-dev-token',
+        ].join('\n')
+      )
+    }
+
     client = Figma.Client({ personalAccessToken: process.env.FIGMA_TOKEN })
   }
   return client
